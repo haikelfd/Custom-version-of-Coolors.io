2e3548aa8357234255b2baad9465e22e9bd56a33
Haikel Fadhloun <51424729+haikelfd@users.noreply.github.com>
2020-04-30 06:29:05 -0700

    Update app.js

--- app.js
+++ app.js
@@ -65,20 +65,5 @@ function colorizeSliders(color, hue, brightness, saturation) {
   )},${scaleBright(0.5)},${scaleBright(1)})`;
   hue.style.backgroundImage = `linear-gradient(to right, rgb(204,7,75),rgb(204,204,75),rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204))`;
 }
-function hslControls(e) {
-  const index =
-    e.target.getAttribute("data-bright") ||
-    e.target.getAttribute("data-sat ") ||
-    e.target.getAttribute("data-hue");
-  let sliders = e.target.parentElement.querySelectorAll('input[type="range"]');
-  const hue = sliders[0];
-  const brightness = sliders[1];
-  const saturation = sliders[2];
-  const bgColor = colorDivs[index].querySelector("h2").innerText;
-  let color = chroma(bgColor)
-    .set("hsl.s", saturation.value)
-    .set("hsl.l", brightness.value)
-    .set("hsl.h", hue.value);
-  colorDivs[index].style.backgroundColor = color;
-}
+ 
 randomColors();
